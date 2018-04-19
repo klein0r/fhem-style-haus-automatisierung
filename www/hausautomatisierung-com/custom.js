52d4c7a251aeda2c6b9ef3cf60c8f2af8cb4224d
Lutz <info@blackbluegl.de>
2018-04-19 15:21:28 +0200

    Style

--- www/hausautomatisierung-com/custom.js
+++ www/hausautomatisierung-com/custom.js
@@ -1,6 +1,19 @@
+
+function GetClock() {
+    d = new Date();
+    nhour = d.getHours();
+    nmin = d.getMinutes();
+    if (nmin <= 9) { nmin = "0" + nmin; }
+    document.getElementById('logo').innerHTML = nhour + ":" + nmin;
+    setTimeout("GetClock()", 1000);
+}
+
 jQuery(document).ready(function ($) {
 
     var themeVersion = '2.6';
+
+    // Add clock
+    window.addEventListener("load",GetClock,false);
 
     // Check für JS-Installation entfernen
     $('#hdr').addClass('js-installed');
@@ -58,4 +71,82 @@ jQuery(document).ready(function ($) {
     $('.roomoverview .col1, .makeTable .col1').each(function(index) {
         $(this).parent().addClass('first-table-column');
     });
+
+    // hide elements by name
+    if(document.URL.indexOf("showall") != -1) {
+        //don't hide anything
+    } else {
+        $("div.devType:contains('-hidden')").parent('td').hide();
+    }
+
+    ;( function( $, window, document, undefined )
+    {
+        'use strict';
+
+        var elSelector		= '#hdr, #logo',
+            elClassHidden	= 'header--hidden',
+            throttleTimeout	= 50,
+            $element		= $( elSelector );
+
+        if( !$element.length ) return true;
+
+        var $window			= $( window ),
+            wHeight			= 0,
+            wScrollCurrent	= 0,
+            wScrollBefore	= 0,
+            wScrollDiff		= 0,
+            $document		= $( document ),
+            dHeight			= 0,
+
+            throttle = function( delay, fn )
+            {
+                var last, deferTimer;
+                return function()
+                {
+                    var context = this, args = arguments, now = +new Date;
+                    if( last && now < last + delay )
+                    {
+                        clearTimeout( deferTimer );
+                        deferTimer = setTimeout( function(){ last = now; fn.apply( context, args ); }, delay );
+                    }
+                    else
+                    {
+                        last = now;
+                        fn.apply( context, args );
+                    }
+                };
+            };
+
+        $window.on( 'scroll', throttle( throttleTimeout, function()
+        {
+            dHeight			= $document.height();
+            wHeight			= $window.height();
+            wScrollCurrent	= $window.scrollTop();
+            wScrollDiff		= wScrollBefore - wScrollCurrent;
+
+            if( wScrollCurrent <= 50 ) // scrolled to the very top; element sticks to the top
+            {
+                $element.removeClass( elClassHidden );
+            }
+            else if( wScrollDiff > 0 && $element.hasClass( elClassHidden ) ) // scrolled up; element slides in
+            {
+                //$element.removeClass( elClassHidden );
+            }
+            else if( wScrollDiff < 0 ) // scrolled down
+            {
+
+                if( wScrollCurrent + wHeight >= dHeight && $element.hasClass( elClassHidden ) ) // scrolled to the very bottom; element slides in
+                {
+                    //$element.removeClass( elClassHidden );
+                }
+                else // scrolled down; element slides out
+                {
+                    $element.addClass( elClassHidden );
+                }
+            }
+
+            wScrollBefore = wScrollCurrent;
+        }));
+
+    })( jQuery, window, document );
 });
