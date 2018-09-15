f5c6966acff27c0318f797667bf831920966affa
Matthias Kleine <matthias@klein0r.de>
2018-09-15 13:11:23 +0200

    Kleine Fehlerbehebungen

--- www/hausautomatisierung-com/custom.js
+++ www/hausautomatisierung-com/custom.js
@@ -19,7 +19,7 @@ function getClock() {
 
 jQuery(document).ready(function ($) {
 
-    var themeVersion = '2.7';
+    var themeVersion = '2.8';
 
     // Check für JS-Installation entfernen
     $('#hdr').addClass('js-installed');
