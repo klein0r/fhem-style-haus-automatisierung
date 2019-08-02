182f3e88733be9ee70891a4e0591b6634401427d
matthias@klein0r.de <Matthias Kleine>
2019-08-02 17:46:04 +0200

    Fixes #102

--- www/pgm2/hausautomatisierung_com.js
+++ www/pgm2/hausautomatisierung_com.js
@@ -71,7 +71,7 @@ jQuery(document).ready(function ($) {
 
 	// Clear spaces
     $('#content .devType, #menu .room a').each(function() {
-    	    $(this).html($(this).html().replace(/&nbsp;/g, ''));
+        $(this).html($(this).html().replace(/&nbsp;/g, ''));
     });
 
     $('#content > br').remove();
